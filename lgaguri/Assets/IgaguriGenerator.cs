822ef8414785c0a139a87ba891ce97f895ed2c74
Unknown <g1727041@st.t-kougei.ac.jp>
2018-07-02 18:07:15 +0900

    いがぐり

---
+++ lgaguri/Assets/IgaguriGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgaguriGenerator : MonoBehaviour {
+    public GameObject igaguriPrefab;
+
+	// Use this for initialization
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject igaguri =
+                Instantiate(igaguriPrefab) as GameObject;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 worldDir = ray.direction;
+            igaguri.GetComponent<IgaguriController>().shoot(
+                 worldDir.normalized*2000);
+        }
+		
+	}
+}
